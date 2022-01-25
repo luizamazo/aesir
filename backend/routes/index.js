b0ba9a264a9173ec5c541fabb6a41bca7e0d450f
Rodrigo Caetano de Andrade <rodrigocaetanodeandrade@gmail.com>
2022-01-24 21:23:24 -0400

    Ajuste padrão rest

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ import {scriptMakerController} from '../controllers/index.js'
 
 const router = express.Router()
 
-router.post('/create-original-script', scriptMakerController.createOriginalScript)
+router.post('/original-scripts', scriptMakerController.createOriginalScript)
 router.post('/generate-script', scriptMakerController.generateScript)
 
 export default router
